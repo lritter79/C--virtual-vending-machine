e233e2a9a770c8f5bbbd8c5c25432542245234c2
Levon Ritter <levong2112@gmail.com>
2019-06-11 14:41:03 -0400

    added unit tests, code ready for Tom to review

---
+++ dotnet/CapstoneTests/Classes/StockTests.cs
@@ -0,0 +1,27 @@
+﻿using CapstoneTests.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone.Classes;
+
+namespace CapstoneTests.Classes
+{
+    [TestClass]
+    public class StockTests
+    {
+        [TestMethod]
+        public void IsStockWorking()
+        {
+            Inventory inventory = new Inventory();
+            Dictionary<string, Item> testDict = new Dictionary<string, Item>();
+            Stock.StockVendingMachine(testDict);
+            foreach (KeyValuePair<string, Item> kvp in testDict)
+            {
+                Assert.AreEqual(kvp.Key, kvp.Value.SlotLocation);
+            }
+        }
+
+
+    }
+}
